592cef3b7cd120703f3ffb1ff6cb644357bf7040
Jared Yang <nociszy@gmail.com>
2024-01-09 09:03:36 -0800

    Update functions.sh

---
+++ exports/functions.sh
@@ -0,0 +1,20 @@
+# ==============================
+# exports/functions
+# ==============================
+mkfile() 
+{ 
+    mkdir -p -- "$1" && touch -- "$1"/"$2" 
+}
+
+# 0. check installed
+# hash records the location of the binary of these commands in a hash table
+check_installed() 
+{
+    hash $1 > /dev/null 2>&1
+    if [ "$?" != "0" ]; then
+        echo -e "\e[1;31m [$1 not found] \e[0m"
+        exit 1
+    else
+        echo -e "\e[1;32m [$1 is installed] \e[0m"
+    fi
+}
