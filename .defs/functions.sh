7f1b1c20b49100a6b8c6299ca6663c184c1a8c06
Jared Yang <nociszy@gmail.com>
2024-01-09 13:01:12 -0800

    Update functions.sh

--- .defs/functions.sh
+++ .defs/functions.sh
@@ -16,7 +16,6 @@ _isExportedToBashrc(){
 _isInstalledPacman() {
     package="$1";
     check="$(sudo pacman -Qs --color always "${package}" | grep "local" | grep "${package} ")";
-    echo ${check}
     if [ -n "${check}" ] ; then
         echo 0; #'0' means 'true' in Bash
         return; #true
@@ -28,7 +27,6 @@ _isInstalledPacman() {
 _installPkgsPacman() {
     toInstall=();
     for pkg; do
-        _isInstalledPacman git
         if [[ $(_isInstalledPacman "${pkg}") == 0 ]]; then
             echo -e "\e[1;33m --[${pkg} is already installed] \e[0m"
             continue;
