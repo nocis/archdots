9a90312fef559e6ba5d2dbc58a5fa26b0c7b3503
Jared Yang <nociszy@gmail.com>
2024-01-09 12:54:32 -0800

    Update functions.sh

--- .defs/functions.sh
+++ .defs/functions.sh
@@ -14,12 +14,8 @@ _isExportedToBashrc(){
 }
 
 _isInstalledPacman() {
-    echo $1
     package="$1";
-    echo $PW | sudo -kS  pacman -Qs --color always "${package}"
-    echo $PW | sudo -kS  pacman -Qs --color always "${package}" | grep "local" | grep "${package}" > /dev/null 2>&1
-    check=$?;
-    echo $check
+    check="$(sudo pacman -Qs --color always "${package}" | grep "local" | grep "${package} ")";
     if [ -n "${check}" ] ; then
         echo 0; #'0' means 'true' in Bash
         return; #true
