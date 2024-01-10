f7aa0dd01d4d79e990c0f64b30cf8753937445f0
Jared Yang <nociszy@gmail.com>
2024-01-09 20:05:02 -0800

    Update functions.sh

--- .defs/functions.sh
+++ .defs/functions.sh
@@ -24,6 +24,19 @@ _isInstalledPacman() {
     return; #false
 }
 
+_isInstalledYay() {
+    package="$1";
+    check="$(sudo yay -Qs --color always "${package}" | grep "local" | grep "${package} ")";
+    if [ -n "${check}" ] ; then
+        echo 0; #'0' means 'true' in Bash
+        return; #true
+    fi;
+    echo 1; #'1' means 'false' in Bash
+    return; #false
+}
+
+
+
 _installPkgsPacman() {
     toInstall=();
     for pkg; do
@@ -40,5 +53,24 @@ _installPkgsPacman() {
 
     # printf "Package not installed:\n%s\n" "${toInstall[@]}";
     echo $PW | sudo -kS  pacman --noconfirm -S "${toInstall[@]}" > /dev/null 2>&1
+    echo -e "\e[1;32m --[${toInstall[@]} installed successfully] \e[0m"
+}
+
+_installPkgsYay() {
+    toInstall=();
+    for pkg; do
+        if [[ $(_isInstalledYay "${pkg}") == 0 ]]; then
+            echo -e "\e[1;33m --[${pkg} is already installed] \e[0m"
+            continue;
+        fi;
+        toInstall+=("${pkg}");
+    done;
+
+    if [[ "${toInstall[@]}" == "" ]] ; then
+        return;
+    fi;
+
+    # printf "Package not installed:\n%s\n" "${toInstall[@]}";
+    echo $PW | sudo -kS  yay --noconfirm -S "${toInstall[@]}" > /dev/null 2>&1
     echo -e "\e[1;32m --[${toInstall[@]} installed successfully] \e[0m"
 }
