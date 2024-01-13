0c78590dfcae0289d4a34fc592cd647c85032383
Jared Yang <nociszy@gmail.com>
2024-01-13 03:34:40 -0800

    Update bashrc-extend.sh

--- exports/bashrc-extend.sh
+++ exports/bashrc-extend.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # Define Editor
 export EDITOR=nvim
+CHROMIUM_FLAGS="--force-device-scale-factor=1.2 --high-dpi-support=1"
 
 # -----------------------------------------------------
 # START STARSHIP
 # -----------------------------------------------------
 eval "$(starship init bash)"
 # -----------------------------------------------------
+
+
