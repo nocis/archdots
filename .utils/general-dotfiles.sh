727c63e5d496277542de1719eb337025b2464c61
Jared Yang <nociszy@gmail.com>
2024-01-13 00:13:30 -0800

    Update general-dotfiles.sh

--- .utils/general-dotfiles.sh
+++ .utils/general-dotfiles.sh
@@ -1,2 +1,3 @@
-_installSymbolicLink  alacritty ~/.config/alacritty ~/.local/dotfiles/config/alacritty/ ~/.config
+_installSymbolicLink alacritty ~/.config/alacritty ~/.local/dotfiles/config/alacritty/ ~/.config
+_installSymbolicLink starship ~/.config/starship.toml  ~/.local/dotfiles/config/starship/starship.toml ~/.config/starship.toml
 
