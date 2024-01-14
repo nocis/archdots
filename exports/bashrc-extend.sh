db52ac45ae91d7dbfc87d9d50fcff4025dec5bb5
Jared Yang <nociszy@gmail.com>
2024-01-13 21:13:37 -0800

    Update bashrc-extend.sh

--- exports/bashrc-extend.sh
+++ exports/bashrc-extend.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# -----------------------------------------------------
+# PFETCH if on wm
+# -----------------------------------------------------
+if [[ $(tty) == *"pts"* ]]; then
+    pfetch
+fi
+
+
 # Define Editor
 export EDITOR=nvim
 # CHROMIUM_FLAGS="--force-device-scale-factor=5.2 --high-dpi-support=1"
