6d035542c0e3d932a2a082da16c778fa3119d97b
Jared Yang <nociszy@gmail.com>
2024-01-13 23:37:18 -0800

    Update bashrc-extend.sh

--- exports/bashrc-extend.sh
+++ exports/bashrc-extend.sh
@@ -18,3 +18,11 @@ eval "$(starship init bash)"
 # -----------------------------------------------------
 
 
+# -----------------------------------------------------
+# nvm
+# -----------------------------------------------------
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+
