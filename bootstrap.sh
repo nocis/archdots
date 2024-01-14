4b7ea5a69d2c03dca90c26ebdee4836e2c280571
Jared Yang <nociszy@gmail.com>
2024-01-13 20:04:42 -0800

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -67,6 +67,8 @@ if [[ ! -f ~/.ssh/id_rsa ]]; then
     echo "Using email $email"
 
     ssh-keygen -t ed25519 -C "$email"
+
+    eval $(ssh-agent -s)
     ssh-add ~/.ssh/id_ed25519.pub
     pub=`cat ~/.ssh/id_ed25519.pub`
 
