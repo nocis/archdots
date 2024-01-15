64dee3bd8f9d90a5ca23189eaa3b85676899b134
nocis <nocis@tju.edu.cn>
2024-01-15 12:59:32 -0800

    add basic nvim plugins

---
+++ config/nvim/lualine.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons", "justinhj/battery.nvim" },
+    event = "VeryLazy",
+
+    opts = function(_, opts)
+      local colors = require("tokyonight.colors").setup({ transform = true })
+      local nvimbatteryn = {
+        function()
+          return require("battery").get_status_line()
+        end,
+        color = { fg = colors.black, bg = colors.blue },
+      }
+      table.insert(opts.sections.lualine_x, nvimbatteryn)
+    end,
+  },
+}
