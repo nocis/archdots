57fb40982a49d2e8beb8b7a32cc6ea4cbcc3c110
Jared Yang <nociszy@gmail.com>
2024-01-20 16:49:08 -0800

    Update cmp.lua

--- config/nvim/lua/plugins/cmp.lua
+++ config/nvim/lua/plugins/cmp.lua
@@ -33,7 +33,14 @@ return {
 				["<C-b>"] = cmp.mapping.scroll_docs(-4),
 				["<C-f>"] = cmp.mapping.scroll_docs(4),
 				["<C-Space>"] = cmp.mapping.complete(),
-				["<C-e>"] = cmp.mapping.abort(),
+				["<C-e>"] = function(fallback)
+					cmp.mapping.abort()
+					cmp.mapping.complete({
+                                            config = {
+                                                sources = {}
+                                            }
+                                        })
+				end,
 				["<CR>"] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 				["<S-CR>"] = cmp.mapping.confirm({
 					behavior = cmp.ConfirmBehavior.Replace,
