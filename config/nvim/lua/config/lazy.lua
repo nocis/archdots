220de6b16e304e3a0796fecdbc11426f4c7357ca
Jared Yang <nociszy@gmail.com>
2024-01-22 11:30:42 -0800

    Update lazy.lua

--- config/nvim/lua/config/lazy.lua
+++ config/nvim/lua/config/lazy.lua
@@ -20,6 +20,8 @@ require("lazy").setup({
 		{ import = "lazyvim.plugins.extras.lang.tailwind" },
 		{ import = "lazyvim.plugins.extras.lang.typescript" },
 		{ import = "lazyvim.plugins.extras.lang.json" },
+		{ import = "lazyvim.plugins.extras.lang.markdown" },
+		{ import = "lazyvim.plugins.extras.lang.cmake" },
 		-- import/override with your plugins
 		{ import = "plugins" },
 	},
