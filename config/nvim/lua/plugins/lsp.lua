2bb45eb720fcda3e4a06c18596517accffb2f04c
Jared Yang <nociszy@gmail.com>
2024-01-23 17:32:49 -0800

    Update lsp.lua

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -136,10 +136,62 @@ return {
 			setup = {
 				tsserver = function()
                                     require("lazyvim.util").lsp.on_attach(function(client, bufnr)
-                                                require('nvim-lsp-ts-utils').setup({
-                                                    filter_out_diagnostics_by_code = { 80001 },
-                                                })
-                                                require('nvim-lsp-ts-utils').setup_client(client)
+							local make_diagnostics_handler = function(original_handler)
+							    return function(...)
+							        local config_or_client_id = select(4, ...)
+   							        local is_new = type(config_or_client_id) ~= "number"
+  							        local result = is_new and select(2, ...) or select(3, ...)
+
+							        local filter_out_diagnostics_by_severity = {}
+							        local filter_out_diagnostics_by_code = { 80001 }
+
+							        -- Convert string severities to numbers
+							        filter_out_diagnostics_by_severity = vim.tbl_map(function(severity)
+							            if type(severity) == "string" then
+							                return u.severities[severity]
+							            end
+
+							            return severity
+							        end, filter_out_diagnostics_by_severity)
+
+							        if #filter_out_diagnostics_by_severity > 0 or #filter_out_diagnostics_by_code > 0 then
+							            local filtered_diagnostics = vim.tbl_filter(function(diagnostic)
+ 							               -- Only filter out Typescript LS diagnostics
+  							              if diagnostic.source ~= "typescript" then
+  							                  return true
+   							             end
+
+							                -- Filter out diagnostics with forbidden severity
+							                if vim.tbl_contains(filter_out_diagnostics_by_severity, diagnostic.severity) then
+							                    return false
+							                end
+
+							                -- Filter out diagnostics with forbidden code
+							                if vim.tbl_contains(filter_out_diagnostics_by_code, diagnostic.code) then
+							                    return false
+							                end
+
+							                return true
+							            end, result.diagnostics)
+
+							            result.diagnostics = filtered_diagnostics
+							        end
+
+							        local config_idx = is_new and 4 or 6
+							        local config = select(config_idx, ...) or {}
+
+							        if is_new then
+							            original_handler(select(1, ...), select(2, ...), select(3, ...), config)
+							        else
+							            original_handler(select(1, ...), select(2, ...), select(3, ...), select(4, ...), select(5, ...), config)
+							        end
+							    end
+							end
+							
+							local PUBLISH_DIAGNOSTICS = "textDocument/publishDiagnostics"
+						        local diagnostics_handler = client.handlers[PUBLISH_DIAGNOSTICS]
+							
+							client.handlers[PUBLISH_DIAGNOSTICS] = make_diagnostics_handler(diagnostics_handler)
                                         end)
                                end,
 			},
