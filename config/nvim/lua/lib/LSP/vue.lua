7cd30fcd4d0dc55f5b97c400750d83caced48516
Jared Yang <nociszy@gmail.com>
2024-01-24 01:09:34 -0800

    Update vue.lua

--- config/nvim/lua/lib/LSP/vue.lua
+++ config/nvim/lua/lib/LSP/vue.lua
@@ -1,11 +1,9 @@
 local util = require("lspconfig.util")
 local have_vue = require("lib.utils.have").have_vue()
 local M = {}
-local root_dir = have_vue and function(...)
-						return require("lspconfig.util").root_pattern("package.json")(...)
-					end
-	or function(...)
-						return ""
-					end
+local root_dir = function(...)
+	return require("lspconfig.util").root_pattern("package.json")(...)
+end
+
 M.root_dir = root_dir
 return M
