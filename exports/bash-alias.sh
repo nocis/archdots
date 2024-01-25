825aa739ba7dbc06ede1029c93851f21d27f2cb1
Jared Yang <nociszy@gmail.com>
2024-01-25 10:15:42 -0800

    Update bash-alias.sh

--- exports/bash-alias.sh
+++ exports/bash-alias.sh
@@ -9,3 +9,4 @@ alias lh='ls -hal'
 
 # workspace
 alias workspace='cd "$HOME/Workspace" && export TERMINAL_WORKSPACE_NAME=workspace && export HISTFILE=${HOME}/.myhistfile.${TERMINAL_WORKSPACE_NAME}'
+alias common='cd "$HOME" && export TERMINAL_WORKSPACE_NAME=common && export HISTFILE=${HOME}/.myhistfile.${TERMINAL_WORKSPACE_NAME}'
