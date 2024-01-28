099729c1dc35fba04fd865b56b160f6cb03ab1da
Jared Yang <nociszy@gmail.com>
2024-01-27 17:11:05 -0800

    Update bash-alias.sh

--- exports/bash-alias.sh
+++ exports/bash-alias.sh
@@ -10,4 +10,4 @@ alias lh='ls -hal'
 # workspace
 alias workspace='cd "$HOME/Workspace" && export TERMINAL_WORKSPACE_NAME=workspace && export HISTFILE=${HOME}/.myhistfile.${TERMINAL_WORKSPACE_NAME}'
 alias common='cd "$HOME" && export TERMINAL_WORKSPACE_NAME=common && export HISTFILE=${HOME}/.myhistfile.${TERMINAL_WORKSPACE_NAME}'
-alias incognito='alacritty && cd "$PWD" && export TERMINAL_WORKSPACE_NAME=incognito && export HISTFILE=/dev/null '
+alias incognito='alacritty -e cd '"$PWD"' && export TERMINAL_WORKSPACE_NAME=incognito && export HISTFILE=/dev/null'
