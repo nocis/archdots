a501fd413f955ff1603bcb865ed5c295077efdc5
Jared Yang <nociszy@gmail.com>
2024-01-31 21:36:54 -0800

    Update general-dotfiles.sh

--- .utils/general-dotfiles.sh
+++ .utils/general-dotfiles.sh
@@ -1,4 +1,5 @@
 _installSymbolicLink alacritty ~/.config/alacritty ~/.local/dotfiles/config/alacritty/ ~/.config
+_installSymbolicLink fontconfig ~/.config/fontconfig ~/.local/dotfiles/config/fontconfig/ ~/.config
 _installSymbolicLink starship ~/.config/starship.toml ~/.local/dotfiles/config/starship/pastel-powerline.toml ~/.config/starship.toml
 _installSymbolicLink nvimPlugin ~/.config/nvim/lua/plugins ~/.local/dotfiles/config/nvim/lua/plugins/ ~/.config/nvim/lua
 _installSymbolicLink nvimConfig ~/.config/nvim/lua/config ~/.local/dotfiles/config/nvim/lua/config/ ~/.config/nvim/lua
