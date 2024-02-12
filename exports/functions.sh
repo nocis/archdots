c84ad43696254acd0f13d16bd23e5fd31e052502
Jared Yang <nociszy@gmail.com>
2024-02-12 13:59:53 -0800

    Update functions.sh

--- exports/functions.sh
+++ exports/functions.sh
@@ -20,9 +20,64 @@ check_installed()
     fi
 }
 
-# 0. find executable
+# 1. find executable
 function findexe() {
   for ARG in $(pacman -Qql $1); do
     [ ! -d $ARG ] && [ -x $ARG ] && echo $ARG;
   done
+}
+
+# 2. cmake-init
+function cmake-init {
+
+if [ ! -d .git ]; then
+  echo "Not a git folder!"
+  exit
+fi
+
+cat <<'EOT' >> build.sh
+#!/bin/bash
+mkdir build
+cd build
+cmake \
+ -DCMAKE_EXPORT_COMPILE_COMMANDS=1 \
+ -DCMAKE_C_COMPILER=/usr/bin/clang \
+ -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
+ -DCMAKE_BUILD_TYPE=Debug \
+ -DCMAKE_INSTALL_PREFIX:PATH="$(dirname $PWD)/dist/" \
+ -DCMAKE_PREFIX_PATH="thirdparty" \
+ # -DQt5_DIR="/path/to/Qt5.6/" \
+ ..
+
+cd ..
+TMP_SRC_DIR="$PWD"
+
+if command -v fd >/dev/null
+then
+    FILES=$(fd -H -t f -F "compile_commands.json" $TMP_SRC_DIR/build/)
+else
+    FILES=$(find $TMP_SRC_DIR/build -name "compile_commands.json")
+fi
+
+TARGETS=($(echo $FILES))
+LINK_NAMES=( $(echo $FILES | sed "s:$TMP_SRC_DIR/build/:$TMP_SRC_DIR/:g") )
+
+for i in ${!TARGETS[*]}
+do
+    if [ "$1" == "-f" ]
+    then
+        ln -srfv ${TARGETS[i]} ${LINK_NAMES[i]}
+    else
+        ln -srv ${TARGETS[i]} ${LINK_NAMES[i]}
+    fi
+done
+
+EOT
+
+cat <<'EOT' >> CMakeLists.txt
+cmake_minimum_required(VERSION 3.20)
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED True)
+EOT
 }
