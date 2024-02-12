71056cc90638f63a79da6c62f424f2024f7ac64a
Jared Yang <nociszy@gmail.com>
2024-02-11 16:50:50 -0800

    Update bash-alias.sh

--- exports/bash-alias.sh
+++ exports/bash-alias.sh
@@ -8,6 +8,6 @@ alias l='ls -CF'
 alias lh='ls -hal'
 
 # workspace
-alias workspace='cd "$HOME/Workspace" && export TERMINAL_WORKSPACE_NAME=workspace && export HISTFILE=${HOME}/.myhistfile.${TERMINAL_WORKSPACE_NAME} && history -c; history -r'
-alias common='cd "$HOME" && export TERMINAL_WORKSPACE_NAME=common && export HISTFILE=${HOME}/.myhistfile.${TERMINAL_WORKSPACE_NAME} && history -c; history -r'
-alias incognito='export TERMINAL_WORKSPACE_NAME=incognito && export HISTFILE=/dev/null && history -c; history -r'
+alias workspace='history -a; cd "$HOME/Workspace" && export TERMINAL_WORKSPACE_NAME=workspace && export HISTFILE=${HOME}/.myhistfile.${TERMINAL_WORKSPACE_NAME} && history -c; history -r'
+alias common='history -a; cd "$HOME" && export TERMINAL_WORKSPACE_NAME=common && export HISTFILE=${HOME}/.myhistfile.${TERMINAL_WORKSPACE_NAME} && history -c; history -r'
+alias incognito='history -a; export TERMINAL_WORKSPACE_NAME=incognito && export HISTFILE=/dev/null && history -c; history -r'
