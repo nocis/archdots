ac61aa6e55218b2185fb97ee9b2776921c5d797f
Jared Yang <nociszy@gmail.com>
2024-02-13 23:50:29 -0800

    Update colorscheme.lua

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,17 @@
 return {
   "folke/tokyonight.nvim",
   opts = {
+    style = "storm",
     transparent = true,
     styles = {
       sidebars = "transparent",
       floats = "transparent",
     },
+    on_highlights = function(highlights, colors) 
+      highlights.Substitute = {
+        bg = "#ff757f",
+        fg = "#e6b793"
+      }
+    end,
   },
 }
