005dc184ac4b8771e8460572f5912639d49e72a6
Jared Yang <nociszy@gmail.com>
2024-09-29 12:07:18 -0700

    Create lsp.lua

---
+++ config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,39 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        cssmodules_ls = {},
+        cssls = {
+          filetypes = { "css", "scss", "less" },
+        },
+        html = {
+          init_options = {
+            configurationSection = { "html", "css", "javascript" },
+            embeddedLanguages = {
+              css = true,
+              javascript = true,
+            },
+            provideFormatter = false,
+          },
+          settings = {
+            css = {
+              lint = {
+                validProperties = {},
+              },
+            },
+          },
+        },
+        yamlls = {
+          settings = {
+            yaml = {
+              keyOrdering = false,
+            },
+          },
+        },
+        glsl_analyzer = {},
+        -- glslls = {},
+      },
+    },
+  },
+}
