e959f8c971e7cfb3d6b4a507e99a978b81199597
Jared Yang <nociszy@gmail.com>
2024-10-08 03:55:17 -0700

    Update keymaps.lua

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,16 +1,17 @@
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
-keymap.set("n", "<C-k>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
-keymap.set("n", "<C-j>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
-keymap.set("n", "<C-h>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
-keymap.set("n", "<C-l>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
-
--- Move to window using the <ctrl> hjkl keys
-keymap.set("n", "<C-Left>", "<C-w>h", { desc = "Go to Left Window", remap = true })
-keymap.set("n", "<C-Down>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
-keymap.set("n", "<C-Up>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
-keymap.set("n", "<C-Right>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+keymap.set("n", "vv", "<C-v>", opts)
+-- keymap.set("n", "<C-k>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+-- keymap.set("n", "<C-j>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+-- keymap.set("n", "<C-h>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
+-- keymap.set("n", "<C-l>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+--
+-- -- Move to window using the <ctrl> hjkl keys
+-- keymap.set("n", "<C-Left>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+-- keymap.set("n", "<C-Down>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+-- keymap.set("n", "<C-Up>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+-- keymap.set("n", "<C-Right>", "<C-w>l", { desc = "Go to Right Window", remap = true })
 
 
 -- Increment/decrement
@@ -86,11 +87,11 @@ vim.keymap.set("n", "<leader>i", function()
 end, { desc = "Toggle Diagnostics" })
 
 -- nabla
-keymap.set("n", "<leader>p", function()
-	require("nabla").popup()
-end, {
-	desc = "nabla popup",
-})
+-- keymap.set("n", "<leader>p", function()
+-- 	require("nabla").popup()
+-- end, {
+-- 	desc = "nabla popup",
+-- })
 
 
 -- git
