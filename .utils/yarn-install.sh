e5adf4386362f3be2a08976d3960560592300fb0
Jared Yang <nociszy@gmail.com>
2025-10-30 02:13:50 +0800

    Update yarn-install.sh

--- .utils/yarn-install.sh
+++ .utils/yarn-install.sh
@@ -10,3 +10,8 @@
 # update yarn
 # yarn set version stable
 # yarn install
+
+
+# we want to install yarn locally
+# npm install yarn -g install base yarn 1.22.22
+# yarn set version stable enable yarn 4 and download yarn.js locally in .yarn/
